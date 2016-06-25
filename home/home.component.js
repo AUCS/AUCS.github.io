502455d660eec759dd5a92616c8f8c6656dde55d
Lucas Sargent <JJ11teen@hotmail.com>
2016-06-25 16:48:57 +0930

    work

---
+++ home/home.component.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Register `home` component, along with its associated controller and template
+angular.
+  module('home').
+  component('home', {
+    templateUrl: 'home/home.template.html',
+    controller: ['Home',
+      function HomeController(Home) {
+        this.phones = Phone.query();
+        this.orderProp = 'age';
+      }
+    ]
+  });
