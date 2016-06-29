6e147766507af9b1065a57998b4c4a874e17d99a
Lucas Sargent <JJ11teen@hotmail.com>
2016-06-29 15:36:07 +0930

    adding latest reviews code for home page

--- testing/core/starify/starify.service.js
+++ testing/core/starify/starify.service.js
@@ -2,13 +2,23 @@ angular.
   module('core.starify').
   factory('Starify', [ function() {
     return function(reviews) {
-      for (i1 = 0; i1 < reviews.length(); i1+=1) {
-        reviews[i].stars = [];
+      if ($.isArray(reviews)) {
+        for (i1 = 0; i1 < reviews.length; i1+=1) {
+          reviews[i].stars = [];
+          for (i2 = 0; i2 < 5; i2+=1) {
+            reviews[i].stars.push({
+              full: reviews[i].rating >= i + 1,
+              half: reviews[i].rating > i,
+              empty: reviews[i].rating <= i
+            });
+          }
+        }
+      } else {
         for (i2 = 0; i2 < 5; i2+=1) {
-          reviews[i].stars.push({
-            full: reviews[i].rating >= i + 1,
-            half: reviews[i].rating > i,
-            empty: reviews[i].rating <= i
+          reviews.stars.push({
+            full: reviews.rating >= i + 1,
+            half: reviews.rating > i,
+            empty: reviews.rating <= i
           });
         }
       }
