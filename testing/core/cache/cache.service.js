2b38554f6592ced4432f1f8a4228b724c7bc726f
Lucas Sargent <JJ11teen@hotmail.com>
2016-06-29 14:07:51 +0930

    adding latest reviews code for home page

--- testing/core/cache/cache.service.js
+++ testing/core/cache/cache.service.js
@@ -2,7 +2,7 @@ angular.
   module('core.cache').
   factory('Cache', ['$resource',
     function($resource) {
-      return $resource('../../data/:cache.json', {}, {
+      return $resource('../data/:cache.json', {}, {
         query: {
           method: 'GET',
           isArray: true
