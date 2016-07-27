f5057d4f654b1a658d775ba5a53c388ca3d41b02
Lucas Sargent <JJ11teen@hotmail.com>
2016-07-28 00:10:05 +0930

    adding about page

--- testing/views/head/head.component.js
+++ testing/views/head/head.component.js
@@ -17,7 +17,8 @@ angular.
       function HeadController($scope, $location, $rootScope) {
         // your controller initialization here ...
         $rootScope.$on("$locationChangeStart", function(event, next, current) {
-          if (next.path() === "/home") {
+          var home_string = "/home";
+          if (next.substr(-home_string.length) === home_string) {
             $scope.templateUrl = 'views/head/head.template-landing.html';
           } else {
             $scope.templateUrl = 'views/head/head.template-main.html';
