661a2191fb05527d6e6dc2e25a1b44d853725c10
Lucas Sargent <JJ11teen@hotmail.com>
2016-07-27 22:50:17 +0930

    adding about page

---
+++ testing/core/core.component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Register `core` component, along with its associated controller and template
+angular.
+  module('core').
+  component('core', {
+    controller: [
+      function CoreController($scope, Cache) {
+        header = '<header class="header"></header>';
+      }]
+  });
