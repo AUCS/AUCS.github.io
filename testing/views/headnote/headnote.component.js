57d8e3ffeb6547d1c7bd4783f6377f96946e3bb2
Lucas Sargent <JJ11teen@hotmail.com>
2016-07-28 22:53:54 +0930

    working on multi-view header

--- testing/views/headnote/headnote.component.js
+++ testing/views/headnote/headnote.component.js
@@ -7,16 +7,17 @@ angular.
     templateUrl: 'views/headnote/headnote.template.html',
     controller: [ '$scope', '$location', '$rootScope',
       function HeadnoteController($scope, $location, $rootScope) {
-				$scope.LoginLabel = $scope.templateUrl;
-        // your controller initialization here ...
-        $rootScope.$on("$locationChangeStart", function(event, next, current) {
+				$scope.Template = $scope.templateUrl;
+        function UpdateHeaderTemplate(event, next, current) {
           var home_string = "/home";
           if (next.substr(-home_string.length) === home_string) {
-            $scope.LoginLabel = 'views/headnote/headnote.template-landing.html';
+            $scope.Template = 'views/headnote/headnote.template-landing.html';
           } else {
-            $scope.LoginLabel = 'views/headnote/headnote.template-main.html';
+            $scope.Template = 'views/headnote/headnote.template-main.html';
           }
-        });
+        }
+        UpdateHeaderTemplate(null, $location.absUrl(), null)
+        $rootScope.$on("$locationChangeStart", UpdateHeaderTemplate(event, next, current));
       }
     ]
   });
