6dce890406b598870ac95b9cda3cfd82619ef56d
Lucas Sargent <JJ11teen@hotmail.com>
2016-07-28 23:57:15 +0930

    working on multi-view header

--- testing/views/headnote/headnote.component.js
+++ testing/views/headnote/headnote.component.js
@@ -10,11 +10,11 @@ angular.
         function UpdateHeaderTemplate(event, next, current) {
           var home_string = "/home";
           if (next.substr(-home_string.length) === home_string) {
-            $('header > div.landing').show();
-            $('header > div.header').hide();
+            $('header > div.landing').fadeIn();       
+            $('header > div.header').fadeOut();
           } else {
-            $('header > div.landing').hide();
-            $('header > div.header').show();
+            $('header > div.landing').fadeOut();
+            $('header > div.header').fadeIn();
           }
         }
         UpdateHeaderTemplate(null, $location.absUrl(), null)
