12bc3140a324b78389f62d1412b52b3a6c13cbd6
Lucas Sargent <JJ11teen@hotmail.com>
2016-08-22 22:34:30 +0930

    adding events

--- testing/views/events/events.component.js
+++ testing/views/events/events.component.js
@@ -2,11 +2,14 @@
 
 // Register `events` component, along with its associated controller and template
 angular.
-  module('events').
-  component('events', {
-    templateUrl: 'views/events/events.template.html',
-    controller: [
-      function EventsController() {
-        
-      }]
-  });
+	module('events').
+	component('events', {
+		templateUrl: 'views/events/events.template.html',
+		controller: [ 'ezfb', '$scope',
+			function EventsController(ezfb, $scope) {
+				var something = ezfb.api('/1549671362000387');
+				var somethingelse = ezfb.api('/1549671362000387/events');
+				$scope.UpcomingEvents = {};
+				$scope.PastEvents = {};
+			}]
+	});
