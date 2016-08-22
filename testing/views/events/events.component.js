d79afd8a290afb8d02365bad399ba059d837cc65
Lucas Sargent <JJ11teen@hotmail.com>
2016-08-22 22:41:05 +0930

    adding events

--- testing/views/events/events.component.js
+++ testing/views/events/events.component.js
@@ -7,8 +7,14 @@ angular.
 		templateUrl: 'views/events/events.template.html',
 		controller: [ 'ezfb', '$scope',
 			function EventsController(ezfb, $scope) {
-				var something = ezfb.api('/1549671362000387');
-				var somethingelse = ezfb.api('/1549671362000387/events');
+				ezfb.api('/1549671362000387', function(res)
+				{
+					var x = 1;
+				});
+				ezfb.api('/1549671362000387/events', function(res)
+				{
+					var x = 3;
+				});
 				$scope.UpcomingEvents = {};
 				$scope.PastEvents = {};
 			}]
